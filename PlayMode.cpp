83f374111c44e54e956ef01f8d6fcb1009392c6a
Weisheng Li <li2546014173@hotmail.com>
2022-11-09 22:01:11 -0500

    fix a small issue caused by merge

--- PlayMode.cpp
+++ PlayMode.cpp
@@ -498,11 +498,6 @@ void PlayMode::draw(glm::uvec2 const &drawable_size) {
 			glm::vec3(H, 0.0f, 0.0f), glm::vec3(0.0f, H, 0.0f),
 			glm::u8vec4(0xff, 0xff, 0xff, 0xff));
 		}
-		
-
-		for (auto &button : buttons)
-			button.draw_button(lines);
-
 	}
 
 	if (notification_active) {
